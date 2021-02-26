7c7a3a20a3a2d8c1a75b02af25a6a6fe84acd8bc
mits <dmitrybaycharov@gmail.com>
2021-02-26 18:09:51 -0500

    Part 1: Building the Form finished

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Let's get this party started!");
+
+const api_key = 'BXOztdBn3GDpzq1xuPcimuRNtN6SHQ4s';
+
+$('form').on('submit', function(e){
+    e.preventDefault();
+    let searchTerm = $(this).children()[0].value;
+    console.log(searchTerm)
+    getGif(searchTerm);
+})
+
+async function getGif(searchTerm) {
+    try {
+      const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchTerm}`);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
